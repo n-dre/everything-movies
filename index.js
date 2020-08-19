cfc241bec4da8554aae6b90df900a4f87dc1adfb
n-dre <contact@oli-branch.com>
2020-08-18 21:50:03 -0400

    changes

---
+++ index.js
@@ -0,0 +1,32 @@
+const Alexa = require('ask-sdk-core')
+const axios = require('axios')
+
+const DirectorIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'DirectorIntent'
+  },
+  handle(handlerInput) {
+    const title = handlerInput.requestEnvelope.request.intent.slots.Title.value
+    const answer = `The director of ${title} is me. HAHA! Just kidding!`
+    const reprompt = 'Would you like information on another movie?'
+    return handlerInput.responseBuilder
+      .speak(answer + reprompt)
+      .reprompt(reprompt)
+      .withShouldEndSession(false)
+      .getResponse();
+  },
+}
+
+const ErrorHandler = {
+  canHandle() {
+    return true
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+    .speak("My bad, didn't catch that. Can you repeat it?")
+    .reprompt("My bad, didn't catch that. Can you repeat it?")
+    .withShouldEndSession(false)
+    .getResponse()
+  },
+}
